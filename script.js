fb252d71bdd5de12757e667a0a7074633d118921
jerryb17 <jerrybhalerao17@gmail.com>
2024-03-11 20:56:53 +0530

    pushing readme

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const Note_Details = [
   { note: 'D', key: 'X', frequency: 293.665, active: false },
   { note: 'Eb', key: 'D', frequency: 311.127, active: false },
   { note: 'E', key: 'C', frequency: 329.628, active: false },
-  { note: 'F', key: 'V', frequency: 349228, active: false },
+  { note: 'F', key: 'V', frequency: 349.228, active: false },
   { note: 'Gb', key: 'G', frequency: 369.994, active: false },
   { note: 'G', key: 'B', frequency: 391.995, active: false },
   { note: 'Ab', key: 'H', frequency: 415.305, active: false },
